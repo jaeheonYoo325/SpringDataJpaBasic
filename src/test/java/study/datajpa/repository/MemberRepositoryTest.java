8ad116a9df9e0c2c2b586a78e6100bd8fd32c4e4
jaeheonYoo325 <yoojh0325@gmail.com>
2021-08-15 19:23:55 +0900

    2021.08.15 : 파라미터 바인딩.

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -2,6 +2,7 @@ package study.datajpa.repository;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.assertj.core.api.Assertions;
@@ -144,6 +145,19 @@ class MemberRepositoryTest {
 		List<MemberDto> memberDto = memberRepository.findMemberDto();
 		for( MemberDto dto : memberDto ) {
 			System.out.println("dto = " + dto);
+		}
+	}
+	
+	@Test
+	public void findByNames() {
+		Member m1 = new Member("AAA", 10);
+		Member m2 = new Member("BBB", 20);
+		memberRepository.save(m1);
+		memberRepository.save(m2);
+		
+		List<Member> result = memberRepository.findByNames(Arrays.asList("AAA", "BBB"));
+		for( Member member : result ) {
+			System.out.println("member = " + member);
 		}
 		
 	}
