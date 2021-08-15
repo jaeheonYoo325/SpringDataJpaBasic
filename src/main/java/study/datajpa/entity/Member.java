6fe138b1f7c356754a8649eca44fb6b56b164f40
jaeheonYoo325 <yoojh0325@gmail.com>
2021-08-15 17:03:43 +0900

    2021.08.15 : 메소드 이름으로 쿼리 생성.

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -32,7 +32,14 @@ public class Member {
 	
 //	protected Member() {}
 
-	public Member(String username) { this.username = username; }
+	public Member(String username) { 
+		this.username = username; 
+	}
+	
+	public Member(String username, int age) {
+		this.username = username;
+		this.age = age;
+	}
 
 	public Member(String username, int age, Team team) {
 		this.username = username;
