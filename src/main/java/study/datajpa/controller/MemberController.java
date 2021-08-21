e88cd187be5b27c7d06792c449d717fbea1a7d4f
jaeheonYoo325 <yoojh0325@gmail.com>
2021-08-22 01:56:32 +0900

    2021.08.22 : 새로운 엔티티를 구별하는 방법.

--- src/main/java/study/datajpa/controller/MemberController.java
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -48,7 +48,7 @@ public class MemberController {
 //		return map;
 	}
 	
-	@PostConstruct
+//	@PostConstruct
 	public void init() {
 		for(int i = 0; i < 100; i++) {
 			memberRepository.save(new Member("user" + i, i));
