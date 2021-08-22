d2ccad4dc6ed64b018582f3f5695dbc5a9a144e3
jaeheonYoo325 <yoojh0325@gmail.com>
2021-08-22 23:13:53 +0900

    2021.08.22 : Query By Example.

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -14,6 +14,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
@@ -420,6 +422,36 @@ class MemberRepositoryTest {
 	@Test
 	public void callCustom() {
 		List<Member> result = memberRepository.findMemberCustom();
+	}
+	
+	@Test
+	public void QueryByExample() {
+		
+		//given
+		Team teamA = new Team("teamA");
+		em.persist(teamA);
+		
+		Member m1 = new Member("m1", 0, teamA);
+		Member m2 = new Member("m2", 0, teamA);
+		em.persist(m1);
+		em.persist(m2);
+		
+		em.flush();
+		em.clear();
+		
+		//when
+		//Probe
+		Member member = new Member("m1");
+		Team team = new Team("teamA");
+		member.setTeam(team);
+		
+		ExampleMatcher matcher = ExampleMatcher.matching()
+				.withIgnorePaths("age");
+		
+		Example<Member> example = Example.of(member, matcher);
+	
+		List<Member> result = memberRepository.findAll(example);
+		Assertions.assertThat(result.get(0).getUsername()).isEqualTo("m1");
 	}
 	
 }
